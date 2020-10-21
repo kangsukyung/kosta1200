03ea2848a30f7445decf90a1232d9befdf4ea51a
jsb202 <tmfql78945@gmail.com>
2020-10-21 19:08:46 +0900

    댓글조회

--- kosta1200/src/kosta1200/todayroom/controller/CommentsController.java
+++ kosta1200/src/kosta1200/todayroom/controller/CommentsController.java
@@ -1,10 +1,82 @@
 package kosta1200.todayroom.controller;
 
-public class CommentsController {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//controller test
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kosta1200.todayroom.action.Action;
+import kosta1200.todayroom.action.ActionForward;
+import kosta1200.todayroom.action.CommentsListAction;
+
+/**
+ * Servlet implementation class CommentsController
+ */
+@WebServlet("/Comments/*" )
+public class CommentsController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public CommentsController() {
+        super();
+    }
+
+	private void doProcess(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		String requestURI = request.getRequestURI();
+		System.out.println(requestURI);
+		String contextPath = request.getContextPath();
+		System.out.println(contextPath);
+    	String command = requestURI.substring(contextPath.length()+10);
+    	System.out.println(command);
+    	
+    	
+    	Action action = null;
+    	ActionForward forward = null;
+    	
+    	if(command.equals("CommentsListAction.do")){
+    		//System.out.println("잘 오고있니");
+    		action = new CommentsListAction();
+    		try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	if(forward != null){
+    		if(forward.isRedirect()){
+    			response.sendRedirect(forward.getPath());
+    		}else{
+    			RequestDispatcher dispacher = request.getRequestDispatcher(forward.getPath());
+    			dispacher.forward(request, response);
+    		}
+    	}
+    	
+    	
+    	
+    	
+    	
 	}
+	
+	
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
 
 }
--- kosta1200/src/kosta1200/todayroom/dao/CommentsDAO.java
+++ kosta1200/src/kosta1200/todayroom/dao/CommentsDAO.java
@@ -1,5 +1,53 @@
 package kosta1200.todayroom.dao;
 
-public class CommentsDAO {
+import java.io.InputStream;
+import java.util.List;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import kosta1200.todayroom.mapper.CommentsMapper;
+import kosta1200.todayroom.vo.CommentsVO;
+
+public class CommentsDAO {
+	private static CommentsDAO dao = new CommentsDAO();
+	
+	public static CommentsDAO getInstance(){
+		return dao;
+	}
+	
+	//mybatis sqlSessionFactory를 쓸 수 있게 만들어줌
+	public SqlSessionFactory getSqlSessionFactory(){
+		String resource = "mybatis-config.xml";
+		InputStream in = null;
+		
+		try {
+			in = Resources.getResourceAsStream(resource);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new SqlSessionFactoryBuilder().build(in);
+	}
+	
+	
+	
+	public List<CommentsVO> listComments(){
+		SqlSession sqlSession = getSqlSessionFactory().openSession();
+		
+		List<CommentsVO> list = null;
+		try {
+			list = sqlSession.getMapper(CommentsMapper.class).listComments();
+			System.out.println("dao에 list잘 오나 : "+list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(sqlSession != null) {
+				sqlSession.close();
+			}
+		}
+		
+		return list;
+	}
 }
