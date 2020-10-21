998173ead31343f4f95bc20ca08952afbf7fc6bd
hanseop <chs20a@naver.com>
2020-10-21 18:55:52 +0900

    10월 21 18시 55분 최한섭

---
+++ kosta1200/src/kosta1200/todayroom/dao/BoardDAO.java
@@ -0,0 +1,56 @@
+package kosta1200.todayroom.dao;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import kosta1200.todayroom.mapper.BoardMapper;
+import kosta1200.todayroom.vo.BoardVO;
+import kosta1200.todayroom.vo.RoomwarmingVO;
+
+public class BoardDAO {
+	private static BoardDAO dao = new BoardDAO();
+	
+	public static BoardDAO getInstance() {
+		return dao;
+	}
+	
+	public SqlSessionFactory getSqlSessionFactory() {
+		String resource = "mybatis-config.xml";
+		InputStream in = null;
+		
+		try {
+			in = Resources.getResourceAsStream(resource);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new SqlSessionFactoryBuilder().build(in);
+		
+	}
+	
+	public int insertBoard(BoardVO board) {
+		int re = -1;
+		SqlSession sqlSession = getSqlSessionFactory().openSession();
+		try {
+			re = sqlSession.getMapper(BoardMapper.class).insertBoard(board);
+			if(re >  0) {
+				sqlSession.commit();
+			}else {
+				sqlSession.rollback();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(sqlSession != null) {
+				sqlSession.close();
+			}
+		}
+		
+		return re;
+	}
+}
---
+++ kosta1200/src/kosta1200/todayroom/mapper/BoardMapper.java
@@ -0,0 +1,8 @@
+package kosta1200.todayroom.mapper;
+
+import kosta1200.todayroom.vo.BoardVO;
+import kosta1200.todayroom.vo.RoomwarmingVO;
+
+public interface BoardMapper {
+	int insertBoard(BoardVO board);
+}
